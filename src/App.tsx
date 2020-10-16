6b1bdb1642e312ac2c176cc566e7562e8f1bcc54
duskcloudxu <duskcloudxu@gmail.com>
2020-10-16 03:16:51 -0700

    pretty test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          start of projectt
+          start of projecttt
         </p>
         <a
           className="App-link"
