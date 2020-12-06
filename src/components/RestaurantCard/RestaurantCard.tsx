420c7a826a88ab0ec18ccf1260da2719b04e84ac
crispgit <39786976+crispgit@users.noreply.github.com>
2020-12-06 15:59:08 -0800

    build project structure and env, add dependencies (#2)
    * build project structure and env, add dependencies
    
    * initialization on the components and pages typescript file
    
    Co-authored-by: duskcloudxu <duskcloudxu@gmail.com>

---
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const RestaurantCard: FunctionComponent<Props> = (props) => {
+  return <div>restaurant card</div>;
+};
+
+export { RestaurantCard };
---
+++ src/pages/RestaurantPage/RestaurantPage.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const RestaurantPage: FunctionComponent<Props> = (props) => {
+  return <div>test</div>;
+};
+
+export { RestaurantPage };
