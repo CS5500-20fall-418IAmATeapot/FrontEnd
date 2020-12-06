941b9cf95a4a98b3ce480e73fde695da0bcd5893
duskcloudxu <duskcloudxu@gmail.com>
2020-12-06 01:04:01 -0800

    initialization on the components and pages typescript file

---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { CartMenu } from "./CartMenu/CartMenu";
+export { RestaurantCard } from "./RestaurantCard/RestaurantCard";
+export { Menu } from "./Menu/Menu";
+export { ItemCard } from "./ItemCard/ItemCard";
