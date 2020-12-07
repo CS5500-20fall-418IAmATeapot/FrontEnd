f6244508c4d775c747d51cf5c214a512f0a1841e
duskcloudxu <duskcloudxu@gmail.com>
2020-12-06 16:51:55 -0800

    update react-router and added routing logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,3 +3,4 @@ export { OrderConfirmPage } from "./OrderConfirmPage/OrderConfirmPage";
 export { OrderHistoryPage } from "./OrderHistoryPage/OrderHistoryPage";
 export { RestaurantPage } from "./RestaurantPage/RestaurantPage";
 export { SearchPage } from "./SearchPage/SearchPage";
+export { NotFoundPage } from "./NotFoundPage/NotFoundPage";
