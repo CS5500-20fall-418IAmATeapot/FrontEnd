f33ec6c324280569584ff02567df018ac84b24e3
crispgit <39786976+crispgit@users.noreply.github.com>
2020-12-12 14:26:58 -0800

    add item card in grid layout (#6)
    * add item card in grid layout
    
    * fix format problems
    
    * modify according to review

--- src/components/Foot/Foot.tsx
+++ src/components/Foot/Foot.tsx
@@ -1,11 +1,9 @@
 import React, { FunctionComponent } from "react";
 import style from "./style.module.css";
-import saber1 from "../../assets/images/saber1.png";
-import saber2 from "../../assets/images/saber2.png";
-import saber3 from "../../assets/images/saber3.png";
-import saber4 from "../../assets/images/saber4.png";
-import { Icon } from "../Icon/Icon";
-import { Route } from "react-router-dom";
+import saber1 from "assets/images/saber1.png";
+import saber2 from "assets/images/saber2.png";
+import saber3 from "assets/images/saber3.png";
+import saber4 from "assets/images/saber4.png";
 
 interface OwnProps {}
 
--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,11 +1,35 @@
 import React, { FunctionComponent } from "react";
+import style from "./style.module.css";
+import { Icon } from "../Icon/Icon";
 
-interface OwnProps {}
+interface OwnProps {
+  name: string;
+  price: number;
+  description: string;
+  imageURL: string;
+  size: number;
+}
 
 type Props = OwnProps;
 
 const ItemCard: FunctionComponent<Props> = (props) => {
-  return <div>Itemcard</div>;
+  return (
+    <div style={{ width: props.size }} className={style.container}>
+      <div className={style.infoArea}>
+        <div className={style.itemName}>{props.name}</div>
+        <div className={style.itemDescription}>{props.description}</div>
+        <div className={style.itemPrice}>{`$${props.price}`}</div>
+        <div className={style.minusBack}>
+          <Icon name={"minus"} size={25} />
+        </div>
+        <div className={style.itemNum}>0</div>
+        <div className={style.plusBack}>
+          <Icon name={"plus"} size={25} />
+        </div>
+      </div>
+      <img className={style.imageArea} src={props.imageURL} alt="item image" />
+    </div>
+  );
 };
 
 export { ItemCard };
