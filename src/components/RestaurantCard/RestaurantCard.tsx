9b388cfde1c7a216ba94217ab8603d0c8cbffcdc
crispgit <39786976+crispgit@users.noreply.github.com>
2020-12-12 18:13:10 -0800

    Add restaurant card (#8)
    * add item card in grid layout
    
    * fix format problems
    
    * modify according to review
    
    * add RestaurantCard
    
    * reformat with prettier
    
    * add image center and fix height
    
    Co-authored-by: duskcloudxu <duskcloudxu@gmail.com>

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -1,11 +1,35 @@
 import React, { FunctionComponent } from "react";
+import style from "./style.module.css";
 
-interface OwnProps {}
+interface OwnProps {
+  name: string;
+  rating: number;
+  fee: number;
+  time: number;
+  imageURL: string;
+  size: number;
+}
 
 type Props = OwnProps;
 
 const RestaurantCard: FunctionComponent<Props> = (props) => {
-  return <div>restaurant card</div>;
+  return (
+    <div style={{ width: props.size }} className={style.container}>
+      <div
+        className={style.imageContainer}
+        style={{ backgroundImage: `URL(${props.imageURL})` }}
+      ></div>
+      <div className={style.infoArea}>
+        <div className={style.restaurantName}>{props.name}</div>
+        <div className={style.deliveryInfo}>
+          {`$${props.fee} Delivery Fee | ${props.time} Min`}
+        </div>
+        <div className={style.ratingBack}>
+          <div className={style.rating}>{props.rating}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { RestaurantCard };
