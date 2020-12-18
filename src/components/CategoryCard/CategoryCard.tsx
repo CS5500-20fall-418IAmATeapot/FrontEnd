36b5144b653a29cd93f2139689de677ab6a42d6b
crispgit <zhoupj1215@hotmail.com>
2020-12-18 14:06:18 -0800

    add link to categories

--- src/components/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react";
 import style from "./style.module.css";
 import { Icon } from "..";
+import { Link } from "react-router-dom";
 
 interface OwnProps {
   name: string;
@@ -13,7 +14,9 @@ type Props = OwnProps;
 const CategoryCard: FunctionComponent<Props> = (props) => {
   return (
     <div style={{ width: props.size + "vw" }} className={style.container}>
-      <Icon name={props.name} size={props.size} />
+      <Link to="/search">
+        <Icon name={props.name} size={props.size} />
+      </Link>
       <div className={style.textArea}>{props.title}</div>
     </div>
   );
